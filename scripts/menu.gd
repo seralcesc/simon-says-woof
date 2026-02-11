34444f29a017c0980b098b072265da31cff8df3f
cmerrod1503 <coraimamr1503@gmail.com>
2026-02-11 09:27:17 +0100

    ajustes

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -2,8 +2,8 @@ extends Node
 
 # Button Start
 func _on_button_pressed() -> void:
-	get_tree().call_deferred("change_scene_to_file", "res://scenes/map01.tscn") # Va al primer mapa
+	get_tree().call_deferred("change_scene_to_file", "res://scenes/map01.tscn") # Go to the first map
 
 # Button Keys
 func _on_button_2_pressed() -> void:
-	get_tree().call_deferred("change_scene_to_file", "res://scenes/keys.tscn")
+	get_tree().call_deferred("change_scene_to_file", "res://scenes/keys.tscn") # Go to the keyboard scene
